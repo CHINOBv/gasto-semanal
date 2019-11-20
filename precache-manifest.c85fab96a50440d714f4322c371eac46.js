d797d585180ce335f4419a941e6b96b8942a1eae
David <david@localhost.localdomain>
2019-11-20 15:06:50 -0600

    Updates

---
+++ precache-manifest.c85fab96a50440d714f4322c371eac46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163303729bf5f9babed833aceb8fc113",
+    "url": "/CHINOBv/gasto-semanal/index.html"
+  },
+  {
+    "revision": "20b5f156c9ad4aaa1a66",
+    "url": "/CHINOBv/gasto-semanal/static/css/main.2710fafd.chunk.css"
+  },
+  {
+    "revision": "bc09d57066ee41c6dfc3",
+    "url": "/CHINOBv/gasto-semanal/static/js/2.ee20a200.chunk.js"
+  },
+  {
+    "revision": "20b5f156c9ad4aaa1a66",
+    "url": "/CHINOBv/gasto-semanal/static/js/main.46fc44c3.chunk.js"
+  },
+  {
+    "revision": "7cb8258defa04ac7142d",
+    "url": "/CHINOBv/gasto-semanal/static/js/runtime-main.475013c9.js"
+  }
+]);
